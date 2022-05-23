0c4b365104e48e51f39d5c40ca7e2f9c0106c5c7
Weberav <104729500+Weberav@users.noreply.github.com>
2022-05-23 11:10:46 +0700

    Add files via upload

---
+++ Remove_Items_From_Download.ps1
@@ -0,0 +1,16 @@
+﻿Get-ChildItem '~\Desktop' | Remove-Item -Include *.log -Recurse
+ 
+ 
+ function date_time()
+ {
+     return (Get-Date -UFormat "%Y-%m-%d_%I-%M-%S").tostring()
+ }
+
+
+
+ $logFileName = "Log_" + (date_time) + ".log"
+
+ $logFileName
+
+
+Get-ChildItem '~\Downloads' | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-1))} | Remove-Item -Force -Verbose 4>&1 | Add-Content ~\Desktop\$logFileName
